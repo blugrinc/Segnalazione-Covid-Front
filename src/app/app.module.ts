9cd6539365bc216bd797f5e1a735f14e32074ee6
Luigi Catania <102974286+blugrinc@users.noreply.github.com>
2022-12-16 12:32:41 +0100

    CssUpdate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SurveyComponent } from './components/survey/survey.component';
+import { PathComponent } from './components/path/path.component';
+import { ExportPageComponent } from './components/exportPage/export-page/export-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyComponent,
+    PathComponent,
+    ExportPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/exportPage/export-page/export-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-export-page',
+  templateUrl: './export-page.component.html',
+  styleUrls: ['./export-page.component.scss']
+})
+export class ExportPageComponent {
+
+}
