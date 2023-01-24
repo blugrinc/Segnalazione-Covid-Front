45a0c5c4c5c207018d1296895a9ab21d54c338f6
Luigi Catania <102974286+blugrinc@users.noreply.github.com>
2023-01-24 18:02:17 +0100

    PDF?

--- src/app/components/path/path25/path25.component.ts
+++ src/app/components/path/path25/path25.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
-import { jsPDF } from 'jspdf';
+import jsPDF from 'jspdf';
 import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-path25',
   templateUrl: './path25.component.html',
-  styleUrls: ['./path25.component.scss'],
+  styleUrls: [ './path25.component.scss' ],
 })
 export class Path25Component {
 
-  user: any;
+  user: any
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   async downloadPDF() {
-    this.authService.user$.subscribe((res) =>{
+    this.authService.user$.subscribe((res) => {
       this.user = res?.user.id
     });
-    const div = document.getElementById('divPDF')!;
-    const doc = new jsPDF('p', 'pt', 'letter');
-    await doc.html(div);
+    const pageOne = document.querySelector('#pageOne') as HTMLSelectElement;
+    const pageTwo = document.querySelector('#pageTwo') as HTMLSelectElement;
+
+    const doc = new jsPDF('p', 'px', [ 1200, 800 ]);
+    await doc.html(pageOne);
+    doc.addPage();
+    await doc.html(pageTwo);
     doc.save(`user${this.user}-report.pdf`);
+
+
   }
 }
